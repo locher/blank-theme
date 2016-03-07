f1a0aeb4149371ed18bc1215b8067ccf781df4ca
locher <loic@herrloc.fr>
2016-03-07 16:34:31 +0100

    Ajout svgstore (sprite)

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,12 @@ module.exports = function(grunt){
 		pkg: grunt.file.readJSON('package.json'),
 		watch: {
 			'default': {
-				files: ['Gruntfile.js', 'sass/*.scss', 'img/svg-dev/*.svg'],
-				tasks: ['sass:dev', 'autoprefixer', 'svgmin']
+				files: ['Gruntfile.js', 'sass/*.scss'],
+				tasks: ['sass:dev', 'autoprefixer']
+			},
+			'svg': {
+				files: ['Gruntfile.js', 'img/svg-dev/*.svg', 'img/svg-dev/sprite/*.svg'],
+				tasks: ['svgmin', 'svgstore']
 			}
 		},
 		sass: {
@@ -52,10 +56,25 @@ module.exports = function(grunt){
                 }]
 	        }
 	    },
+		svgstore: {
+		    options: {
+		      prefix : 'icon-',
+		      svg: {
+		        viewBox : '0 0 100 100',
+		        xmlns: 'http://www.w3.org/2000/svg'
+		      }
+		    },
+		    your_target: {
+		      files:{
+		      	'img/svg-prod/sprite/svgs.svg' : ['img/svg-dev/*.svg'],
+		      },
+		    },
+		},
 	});
 	
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-svgmin');
+	grunt.loadNpmTasks('grunt-svgstore');
 }
